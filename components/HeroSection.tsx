d9dffe16dd3f5d74b40aa2b140d73eb55cdce4cb
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-12-03 01:16:04 +0800

    escaped entity

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ import gradient from "@/public/5.png"
 const HeroSection = () => {
     return ( 
         <section id="hero__section" className="flex-col gap-4">
-            <p className="text-2xl lg:text-5xl font-black drop-shadow">hey there, i'm patty</p>
+            <p className="text-2xl lg:text-5xl font-black drop-shadow">hey there, i&apos;m patty</p>
             <div className="w-1/2 p-2 justify-center items-center rounded-lg drop-shadow-md text-sm lg:text-lg text-center">
                 <p><span>Designer</span> and <span>Developer</span></p>
             </div>
